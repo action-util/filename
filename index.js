cde43d9b67468baa92003a1fee3db6474f2896dc
Jesse A. Tov <jesse.tov@gmail.com>
2020-01-12 01:37:42 -0600

    DSFLKSDJF

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ try {
   const path = core.getInput('path');
   console.log(`path: “${path}”`);
 
-  if (path.match(/[/]/) {
-    core.setOutput('dirname', path.replace(/[/][^/]*$/, '');
-    core.setOutput('basename', path.replace(/.*[/]/, '');
+  if (path.match(/[\/]/) {
+    core.setOutput('dirname', path.replace(/[\/][^\/]*$/, '');
+    core.setOutput('basename', path.replace(/.*[\/]/, '');
   } else {
     core.setOutput('dirname', '.');
     core.setOutput('basename', path);
