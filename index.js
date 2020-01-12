e67e8bece502f953ae733eec110ab09ebef3f262
Jesse A. Tov <jesse.tov@gmail.com>
2020-01-12 01:19:48 -0600

    Trying to split.

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ try {
   const path = core.getInput('path');
   console.log(`path: “${path}”`);
 
-  const basename = path.replace(/.*\//, '');
-  const dirname = path.replace(/\/[^\/]*$/, '');
+  const match = path.match(/^(.*)\/([^\\]*)$/);
 
-  core.setOutput("dirname", dirname);
-  core.setOutput("basename", basename);
+  if (match) {
+    core.setOutput('dirname', match[2]);
+    core.setOutput('basename', match[3]);
+  } else {
+    core.setOutput('dirname', '.');
+    core.setOutput('basename', path);
+  }
 
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
